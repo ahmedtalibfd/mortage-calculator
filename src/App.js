e01b36921756c4c5d3ab294d089986412b97b49e
Ahmedtalib <Ahmedtalibfd@gmail.com>
2022-10-18 13:42:01 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import MortageCalculator from './mortage-calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mortage Calculator</h1>
+      <MortageCalculator />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/mortage-calculator/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Section from "./Section";
+
+const MortageCalculator = () => {
+return <div>
+    <Section ></Section>
+    <Section ></Section>
+</div>
+}
+
+export default MortageCalculator;
