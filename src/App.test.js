1bf819a8b167c2423189deed36bca4746b7b7582
Ahmedtalib <Ahmedtalibfd@gmail.com>
2022-10-20 02:27:43 -0400

    adding test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,44 @@
-import { render, screen } from '@testing-library/react';
+import { logRoles, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+describe('Checking app functionality', () => {
+ it('check table is getting displayed with default values',()=>{
+ render(<App />);
+ 
+  const headerRow = screen.getByRole('row',{name:'Category Ammortization Period'});
+  const noOfPaymentsRow = screen.getByRole('row',{name:'Number of Payments 300'});
+  const monthlyPaymentRow = screen.getByRole('row',{name:'Mortage Payment 584.59'});
+  const principalPaymentsRow = screen.getByRole('row',{name:'Principal Payments 100000'});
+  const IntrestPaymentsRow = screen.getByRole('row',{name:'Intrest Payments 75377'});
+  expect(headerRow).toBeVisible()
+  expect(noOfPaymentsRow).toBeVisible()
+  expect(monthlyPaymentRow).toBeVisible()
+  expect(principalPaymentsRow).toBeVisible()
+  expect(IntrestPaymentsRow).toBeVisible()
+
+ })
+ it('change Inputs and check the values displayed in the table',()=>{
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  
+   const mortageAmountInput = screen.getByLabelText('Mortage Amount')
+   userEvent.type(mortageAmountInput,'200000')
+   const intrestAmountInput = screen.getByLabelText('Interest Rate')
+   userEvent.type(intrestAmountInput,'10')
+   // we should add more changes here
+   const calcButt =screen.getByRole("button",{name:'Calculate'})
+   userEvent.click(calcButt)
+   const headerRow = screen.getByRole('row',{name:'Category Ammortization Period'});
+   const noOfPaymentsRow = screen.getByRole('row',{name:'Number of Payments 300'});
+   const monthlyPaymentRow = screen.getByRole('row',{name:'Mortage Payment 1817.40'});
+   const principalPaymentsRow = screen.getByRole('row',{name:'Principal Payments 200000'});
+   const IntrestPaymentsRow = screen.getByRole('row',{name:'Intrest Payments 345220.00'});
+   expect(headerRow).toBeVisible()
+  expect(noOfPaymentsRow).toBeVisible()
+  expect(monthlyPaymentRow).toBeVisible()
+  expect(principalPaymentsRow).toBeVisible()
+  expect(IntrestPaymentsRow).toBeVisible()
+ 
+  })
+  
 });
