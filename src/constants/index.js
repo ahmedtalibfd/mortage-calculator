d9293f9f4011fa7e5762c833c31998324fcc1d09
Ahmedtalib <Ahmedtalibfd@gmail.com>
2022-10-20 00:25:37 -0400

    Adding Constants

---
+++ src/constants/index.js
@@ -0,0 +1,57 @@
+export const yearsDropdownVals =[
+    { value: 0, option: '' },
+    { value: 1, option: '1 Years' },
+    { value: 2, option: '2 Years' },
+    { value: 3, option: '3 Years' },
+    { value: 4, option: '4 Years' },
+    { value: 5, option: '5 Years' },
+    { value: 6, option: '6 Years' },
+    { value: 7, option: '7 Years' },
+    { value: 8, option: '8 Years' },
+    { value: 9, option: '9 Years' },
+    { value: 10, option: '10 Years' },
+    { value: 11, option: '11 Years' },
+    { value: 12, option: '12 Years' },
+    { value: 13, option: '13 Years' },
+    { value: 14, option: '14 Years' },
+    { value: 15, option: '15 Years' },
+    { value: 16, option: '16 Years' },
+    { value: 17, option: '17 Years' },
+    { value: 18, option: '18 Years' },
+    { value: 19, option: '19 Years' },
+    { value: 20, option: '20 Years' },
+    { value: 21, option: '21 Years' },
+    { value: 22, option: '22 Years' },
+    { value: 23, option: '23 Years' },
+    { value: 24, option: '24 Years' },
+    { value: 25, option: '25 Years' },
+    { value: 26, option: '26 Years' },
+    { value: 27, option: '27 Years' },
+    { value: 28, option: '28 Years' },
+    { value: 29, option: '29 Years' },
+    { value: 30, option: '30 Years' }
+  ]
+
+
+  export const monthsDropdownVals = [
+    { value: 0, option: '0 Months' },
+    { value: 1, option: '1 Months' },
+    { value: 2, option: '2 Months' },
+    { value: 3, option: '3 Months' },
+    { value: 4, option: '4 Months' },
+    { value: 5, option: '5 Months' },
+    { value: 6, option: '6 Months' },
+    { value: 7, option: '7 Months' },
+    { value: 8, option: '8 Months' },
+    { value: 9, option: '9 Months' },
+    { value: 10, option: '10 Months' },
+    { value: 11, option: '11 Months' },
+    { value: 12, option: '12 Months' }
+  ]
+
+
+  export const NUMBER_OF_PAYMENTS = 'Number of Payments'
+  export const MONTHLY_PAYMENT = 'Mortage Payment'
+  export const PRINCIPAL_PAYMENTS = 'Principal Payments'
+  export const INTREST_PAYMENTS = 'Intrest Payments'
+  export const TOTAL_COST = 'Total Cost'
